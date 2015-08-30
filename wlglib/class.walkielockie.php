2712f2990637678b3d1152a087bc475623d1e0ff
Hakan Bilgin <hbi99@hotmail.com>
2015-08-31 00:15:53 +0200

    css updates

---
+++ wlglib/class.walkielockie.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once('config.php');
+include_once('class.error.php');
+
+class WLG {
+
+	function __construct() {
+		global $ERR;
+
+	}
+
+	public function register() {
+		global $ERR;
+		
+	}
+
+	public function unregister() {
+		global $ERR;
+		
+	}
+
+}
+
+$WLG = new WLG;
+
+?>
