715b1b1d3147813b0fa0262e320c4ba822df5787
Hakan Bilgin <hbi@longscript.com>
2019-01-31 00:04:48 +0100

    fixes

--- v2/gulpfile.js
+++ v2/gulpfile.js
@@ -12,9 +12,9 @@ const hljs = require("highlight.js")
 
 const html_data = {
 	version: {
-		defiant: "v2.2.0",
+		defiant: "v2.2.1",
 		junior: "v1.1.4",
-		jupyter: "v1.0.1",
+		jupyter: "v1.0.5",
 		rebellious: "1.0.1",
 	}
 };
